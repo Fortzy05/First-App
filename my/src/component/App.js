ade8b8d762131d534d633bacef937206111fe46c
fortune <omorodion.fortune@gmail.com>
2021-12-11 15:38:39 +0100

    modified components

--- my/src/component/App.js
+++ my/src/component/App.js
@@ -1,8 +1,11 @@
 import React from "react";
+import SearchBar from "./SearchBar";
 
 class App extends React.Component {
     render(){
-        return <div>my app</div>
+        return <div className='ui container' style={{marginTop: '10px'}}>
+            <SearchBar/>
+        </div>
     }
         
     
